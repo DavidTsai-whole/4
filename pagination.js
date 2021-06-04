59d8262479dddd71127440c54d82f3b359f77b6a
蔡皓全 <a22646185@gmail.com>
2021-06-04 19:36:11 +0800

    5

--- pagination.js
+++ pagination.js
@@ -9,7 +9,7 @@ export default {
         </a>
       </li>
       <li class="page-item"  v-for="item in page.total_pages" :key="item" :class="{active:item === page.current_page}"><a class="page-link" href="#" @click="$emit('page-change',item)">{{item}}</a></li>
-      <li class="page-item":class="{disabled:!page.has_next}" @click="$emit('page-change',page.current_page +1)">
+      <li class="page-item" :class="{disabled:!page.has_next}" @click="$emit('page-change',page.current_page +1)">
         <a class="page-link" href="#" aria-label="Next">
           <span aria-hidden="true">&raquo;</span>
         </a>
--- product.js
+++ product.js
@@ -5,106 +5,111 @@ let productModal = {};
 let deleteModal = {};
 
 const app = createApp({
-    data() {
-        return {
-            apiUrl: 'https://vue3-course-api.hexschool.io',
-            apiPath: 'jamestsai',
-            products: [],
-            tempProduct: {},
-            pagination: {},
-            isNew: false,
-        }
-    },
-    components: {
-        pagination
-    },
-    methods: {
-        getData(page = 1) {
-            const url = `${this.apiUrl}/api/${this.apiPath}/admin/products?page=${page}`;
-            axios.get(url).then(res => {
-                if (res.data.success) {
-                    this.products = res.data.products;
-                    this.pagination = res.data.pagination;
-                }
-                else {
-                    alert('讀取失敗')
-                }
-            }).catch(error => {
-                consolo.log(error)
-            });
-        },
-        openModal(isNew, item) {
-            this.isNew = isNew;
-            if (this.isNew) {
-                this.tempProduct = {};
-                productModal.show();
-            } else {
-                this.tempProduct = { ...item };
-                productModal.show();
-            }
-        },
-        delModal(item) {
-            this.tempProduct = item;
-            deleteModal.show();
-        },
+  data() {
+    return {
+      api: {
+        apiUrl: 'https://vue3-course-api.hexschool.io',
+        apiPath: 'jamestsai',
+      },
 
-        updateProduct(item) {
-            let api = `${this.apiUrl}/api/${this.apiPath}/admin/product`;
-            let method = 'post';
-            if (!this.isNew) {
-                api = `${this.apiUrl}/api/${this.apiPath}/admin/product/${item.id}`;
-                method = 'put';
-            }
-            axios[method](api, { data: item }).then(res => {
-                if (res.data.success) {
-                    productModal.hide();
-                    this.getData();
-                }
-                else {
-                    alert(res.data.message)
-                }
-            }).catch(error => {
-                consolo.log(error)
-            })
-            this.getData();
-        },
-        deleteProduct() {
-            const api = `${this.apiUrl}/api/${this.apiPath}/admin/product/${this.tempProduct.id}`;
-            axios.delete(api).then(res => {
-                if (res.data.success) {
-                    this.getData();
-                    deleteModal.hide();
-                }
-            }).catch(error => {
-                consolo.log(error)
-            })
-        }
-    },
-    //mounted是連dom元素都取得了
-    mounted() {
-        const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
-        if (token === '') {
-            alert('請重新登入');
-            window.location = 'index.html';
-        }
-        axios.defaults.headers.common.Authorization = token;
-        productModal = new bootstrap.Modal(document.getElementById('productModal'));
-        deleteModal = new bootstrap.Modal(document.getElementById('delProductModal'));
 
-        this.getData();
+
+      products: [],
+      tempProduct: {},
+      pagination: {},
+      isNew: false,
     }
+  },
+  components: {
+    pagination
+  },
+  methods: {
+    getData(page = 1) {
+      const url = `${this.api.apiUrl}/api/${this.api.apiPath}/admin/products?page=${page}`;
+      axios.get(url).then(res => {
+        if (res.data.success) {
+          this.products = res.data.products;
+          this.pagination = res.data.pagination;
+        }
+        else {
+          alert('讀取失敗')
+        }
+      }).catch(error => {
+        consolo.log(error)
+      });
+    },
+    openModal(isNew, item) {
+      this.isNew = isNew;
+      if (this.isNew) {
+        this.tempProduct = {};
+        productModal.show();
+      } else {
+        this.tempProduct = { ...item };
+        productModal.show();
+      }
+    },
+    delModal(item) {
+      this.tempProduct = item;
+      deleteModal.show();
+    },
+
+    updateProduct(item) {
+      let api = `${this.api.apiUrl}/api/${this.api.apiPath}/admin/product`;
+      let method = 'post';
+      if (!this.isNew) {
+        api = `${this.api.apiUrl}/api/${this.api.apiPath}/admin/product/${item.id}`;
+        method = 'put';
+      }
+      axios[method](api, { data: item }).then(res => {
+        if (res.data.success) {
+          productModal.hide();
+          this.getData();
+        }
+        else {
+          alert(res.data.message)
+        }
+      }).catch(error => {
+        consolo.log(error)
+      })
+      this.getData();
+    },
+    deleteProduct() {
+      const api = `${this.api.apiUrl}/api/${this.api.apiPath}/admin/product/${this.tempProduct.id}`;
+      axios.delete(api).then(res => {
+        if (res.data.success) {
+          this.getData();
+          deleteModal.hide();
+        }
+      }).catch(error => {
+        consolo.log(error)
+      })
+    }
+  },
+  //mounted是連dom元素都取得了
+  mounted() {
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+    if (token === '') {
+      alert('請重新登入');
+      window.location = 'index.html';
+    }
+    axios.defaults.headers.common.Authorization = token;
+    productModal = new bootstrap.Modal(document.getElementById('productModal'));
+    deleteModal = new bootstrap.Modal(document.getElementById('delProductModal'));
+
+    this.getData();
+  }
 });
 //元件是內層，用props接收外層資料，用emit操縱外層事件,並建議把資料再帶回去(參數)
 app.component('productModal', {
-    props: ['tempProduct'],
-    template: `<div id="productModal" ref="productModal" class="modal fade" tabindex="-1" aria-labelledby="productModalLabel"
+  props: ['tempProduct', 'api'],
+  template: `<div id="productModal" ref="productModal" class="modal fade" tabindex="-1" aria-labelledby="productModalLabel"
     aria-hidden="true">
     <div class="modal-dialog modal-xl">
       <div class="modal-content border-0">
         <div class="modal-header bg-dark text-white">
           <h5 id="productModalLabel" class="modal-title">
-            <span>新增產品</span>
-            <span>編輯產品</span>
+            <span v-if="tempProduct.title">{{tempProduct.title}}</span>
+            <span v-else>新增商品</span>
           </h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
@@ -120,37 +125,10 @@ app.component('productModal', {
 
               </div>
               <div class="mb-1">
-              <input type="file" class="form-control" @change="upload" id="file"/>   
+              <input type="file" class="form-control" ref="uploadDom" @change="upload"/> 
+              <img class="img-fluid" :src="tempProduct.imgUpload" alt="">  
               </div>
               
-              <div class="mb-1">多圖新增</div>
-              <div v-if="Array.isArray(tempProduct.imagesUrl)">
-                <div class="mb-1" v-for="(image, key) in tempProduct.imagesUrl" :key="key">
-                  <div class="form-group">
-                    <label for="imageUrl">圖片網址</label>
-                    <input type="text" class="form-control" placeholder="請輸入圖片連結"
-                      v-model="tempProduct.imagesUrl[key]">
-                  </div>
-                  <img class="img-fluid" :src="image">
-                </div>
-                <div v-if="!tempProduct.imagesUrl.length || tempProduct.imagesUrl[tempProduct.imagesUrl.length-1] ">
-                  <button class="btn btn-outline-primary btn-sm d-block w-100"
-                    @click="tempProduct.imagesUrl.push('')">
-                    新增圖片
-                  </button>
-                </div>
-                <div v-else>
-                  <button class="btn btn-outline-danger btn-sm d-block w-100" @click="tempProduct.imagesUrl.pop()">
-                    刪除圖片
-                  </button>
-                </div>
-              </div>
-              <div v-else>
-                <button class="btn btn-outline-primary btn-sm d-block w-100" @click="createImages">
-                  新增陣列圖片
-                </button>
-              </div>
-
             </div>
             <div class="col-sm-8">
               <div class="form-group">
@@ -217,18 +195,24 @@ app.component('productModal', {
       </div>
     </div>
   </div>`,
-    methods: {
-        createImages() {
-            this.tempProduct.imagesUrl = ['']
-        },
-        upload(){
-          
-        }
+
+  methods: {
+    upload() {
+      const file = this.$refs.uploadDom.files[0]
+      const formData = new FormData();
+      formData.append('file-to-upload', file);
+      axios.post(`${this.api.apiUrl}/api/${this.api.apiPath}/admin/upload`, formData).then(res => {
+        this.tempProduct.imgUpload = res.data.imageUrl
+
+      }).catch(error => {
+        console.log(error)
+      })
     }
+  }
 });
 app.component('deleteModal', {
-    props:['tempProduct'],
-    template: `<div id="delProductModal" ref="delProductModal" class="modal fade" tabindex="-1"
+  props: ['tempProduct'],
+  template: `<div id="delProductModal" ref="delProductModal" class="modal fade" tabindex="-1"
     aria-labelledby="delProductModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content border-0">
@@ -250,7 +234,6 @@ app.component('deleteModal', {
             確認刪除
           </button>
         </div>
-      </div>
     </div>
   </div>`
 })
